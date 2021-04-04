326d9ec11ebdeee0d7556f3c2faa23a49b22fce6
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-04-03 21:02:32 -0500

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,12 @@
 # Client configuration file (w/ Puppet)
 # using Puppet to make changes to our configuration file
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-}
-
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/holberton',
+file {'~/.ssh/config':
+  ensure  => present,
+  replace => 'yes',
+  path    => '~/.ssh/config',
+  content => 'Host *
+     HostName 35.196.52.46
+     User ubuntu
+     IdentityFile ~/.ssh/holberton',
+  mode    => '7000',
 }
