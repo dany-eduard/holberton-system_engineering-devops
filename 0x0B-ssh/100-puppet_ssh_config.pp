5478c39982138901a80bb9753c0ae38cd2e0cfa4
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-04-03 21:01:12 -0500

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,5 @@
 # Client configuration file (w/ Puppet)
+# using Puppet to make changes to our configuration file
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
